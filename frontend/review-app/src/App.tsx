46eac39d241142968f40a8c91a45cf28b8a9b4e8
Nohasi <hamood-ah19@hotmail.com>
2023-05-09 11:20:40 +0530

    Basic file structure set

---
+++ frontend/review-app/src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* NAVBAR */}
+
+        {/* ROUTING */}
+
+        {/* FOOTER */}
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/review-app/src/components/info_page/InfoPage.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function InfoPage(){
+    return (
+        <>
+        {/* ITEM DETAILS */}
+        {/* REVIEWS */}
+        </>
+    );
+}
