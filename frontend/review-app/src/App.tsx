695e1e5b747b41b3e9247ccbdd97475434a46398
Nohasi <hamood-ah19@hotmail.com>
2023-05-09 14:12:39 +0530

    Added footer to page

--- frontend/review-app/src/App.tsx
+++ frontend/review-app/src/App.tsx
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import PageNavbar from './components/global/PageNavbar';
 import InfoPage from './components/info_page/InfoPage';
+import PageFooter from './components/global/PageFooter';
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
         {/* ROUTING */}
         <InfoPage />
 
-        {/* FOOTER */}
+        <PageFooter/>
     </div>
   );
 }
---
+++ frontend/review-app/src/components/global/PageFooter.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function PageFooter() {
+    return(
+        <>
+            <footer className="PageFooter">
+                Mohammed Alhosani ©
+            </footer>
+        </>
+    );
+}
+export default PageFooter;
