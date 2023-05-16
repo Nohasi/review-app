fa7376efe8354c8facfc43c1efe61952e1dc8ad9
Nohasi <hamood-ah19@hotmail.com>
2023-05-16 14:53:03 +0530

    Removed unnecessary line

--- frontend/review-app/src/App.tsx
+++ frontend/review-app/src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import PageNavbar from './components/global/PageNavbar';
 import InfoPage from './components/info_page/InfoPage';
