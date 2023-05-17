6052557985f6ef4cc43eb9fe87849ff968e8aad9
Nohasi <hamood-ah19@hotmail.com>
2023-05-17 15:13:11 +0530

    Fixed issue with navbar link

--- frontend/review-app/src/components/global/PageNavbar.tsx
+++ frontend/review-app/src/components/global/PageNavbar.tsx
@@ -4,15 +4,15 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
 import '../../App.css';
-import { Link } from 'react-router-dom';
+import { Link } from '@mui/material';
 
 export default function PageNavbar() {
   return (
     <Box sx={{ flexGrow: 1, paddingBottom: '75px' }}>
       <AppBar style={{ background: '#808080' }}>
         <Toolbar>
-          <Link to={{pathname: '/itemlist'}}>
-            <Button color="inherit" className='Navbar-button'>View All Items</Button>
+          <Link href='itemlist'>
+            <Button color="inherit" sx={{color: "white"}} className='Navbar-button'>View All Items</Button>
           </Link>
         </Toolbar>
       </AppBar>
