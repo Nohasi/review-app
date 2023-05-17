cd97924cd33a3638ebb036fd80317028afa4ba6f
Nohasi <hamood-ah19@hotmail.com>
2023-05-17 11:35:01 +0530

    Added item card

--- frontend/review-app/src/components/item_list/ItemList.tsx
+++ frontend/review-app/src/components/item_list/ItemList.tsx
@@ -1,10 +1,12 @@
 import { Container } from "@mui/system";
+import ItemCard from "./ItemCard";
 
 function ItemList() {
     return (
         <div>
             <Container>
                 <h1> Item List</h1>
+                <ItemCard />
             </Container>
         </div>
     );
