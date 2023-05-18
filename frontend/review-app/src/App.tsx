bd60795a0d052501fbd2655731ee74c0ccf6c035
Nohasi <hamood-ah19@hotmail.com>
2023-05-18 11:29:33 +0530

    returned router to BrowserRouter from HashRouter

--- frontend/review-app/src/App.tsx
+++ frontend/review-app/src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 import PageNavbar from './components/global/PageNavbar';
 import InfoPage from './components/info_page/InfoPage';
 import PageFooter from './components/global/PageFooter';
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ItemList from './components/item_list/ItemList';
 import ReviewForm from './components/add_review/ReviewForm';
 
@@ -12,14 +12,14 @@ function App() {
     <div className="App">
         <PageNavbar />
 
-        <HashRouter>
+        <BrowserRouter>
           <Routes>
             <Route path="" element={<ItemList />} />
             <Route path="/iteminfo" element={<InfoPage />} />
             <Route path="/itemlist" element={<ItemList />} />
             <Route path="/addreview" element={<ReviewForm />} />
           </Routes>
-        </HashRouter>
+        </BrowserRouter>
 
         <PageFooter/>
     </div>
